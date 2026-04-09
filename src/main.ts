15bf5e70eb5aa60b3fea56dcc72ac8e226e526af
Mohamed Hakeem <m@gmail.com>
2026-04-09 15:20:37 +0200

    add Cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
+ app.enableCors({
+  origin: 'http://localhost:3000',     // Next.js frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,                    // لو هتستخدم cookies في المستقبل
+});
   await app.listen(4000);
   console.log('🚀 Application is running on: http://localhost:4000');
 }
